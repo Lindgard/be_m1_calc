7fdc3256f31dc9903634d847844f6fa21dc4c0bb
ALP <a.b.lindgard@gmail.com>
2026-01-05 12:33:23 +0100

    first commit, included class with start of methods for a calculator

---
+++ Classes/Calculator.cs
@@ -0,0 +1,40 @@
+public class Calculator
+{
+    //* Integer-version of methods
+    int AddNumbers(int a, int b)
+    {
+        return a + b;
+    }
+
+    int SubtractNumbers(int a, int b)
+    {
+        return a - b;
+    }
+    int MultiplyNumbers(int a, int b)
+    {
+        return a * b;
+    }
+    int DivideNumbers(int a, int b)
+    {
+        return a / b;
+    }
+
+    //* Double-version of methods
+
+    double AddNumbers(double a, double b)
+    {
+        return a + b;
+    }
+    double SubtractNumbers(double a, double b)
+    {
+        return a - b;
+    }
+    double MultiplyNumbers(double a, double b)
+    {
+        return a * b;
+    }
+    double DivideNumbers(double a, double b)
+    {
+        return a / b;
+    }
+}
