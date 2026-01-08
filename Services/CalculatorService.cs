3e98dfd05508cc1dd92acf760876f4fb30db1c83
ALP <a.b.lindgard@gmail.com>
2026-01-08 13:09:43 +0100

    modified switch to include the method type in the results

--- Services/CalculatorService.cs
+++ Services/CalculatorService.cs
@@ -1,3 +1,5 @@
+using System.Runtime.CompilerServices;
+
 public class CalculatorService
 {
     private Calculator _calculator;
@@ -12,27 +14,64 @@ public class CalculatorService
         return double.TryParse(input, out number);
     }
 
-    public bool PerformCalculation(double num1, string operation, double num2, out double result)
+    public bool PerformCalculation(double num1, string operation, double num2, out double result, out string methodType)
     {
         result = 0.0;
+        methodType = "";
+
+        bool useIntVersion = (num1 % 1 == 0) && (num2 % 1 == 0);
 
         switch (operation.ToLower())
         {
             case "+":
             case "add":
-                result = _calculator.AddNumbers(num1, num2);
+                if (useIntVersion)
+                {
+                result = _calculator.AddNumbers((int)num1, (int)num2);
+                methodType = "int";
+                }
+                else
+                {
+                    result = _calculator.AddNumbers(num1, num2);
+                    methodType = "double";
+                }
                 return true;
             case "-":
             case "subtract":
-                result = _calculator.SubtractNumbers(num1, num2);
+                if(useIntVersion)
+                {
+                    result = _calculator.SubtractNumbers((int)num1, (int)num2);
+                    methodType = "int";
+                }
+                else
+                {
+                    result = _calculator.SubtractNumbers(num1, num2);
+                    methodType = "double";
+                }
                 return true;
             case "*":
             case "multiply":
-                result = _calculator.MultiplyNumbers(num1, num2);
+                if(useIntVersion)
+                {
+                    result = _calculator.MultiplyNumbers((int)num1, (int)num2);
+                    methodType = "int";
+                }
+                else
+                {
+                    result = _calculator.MultiplyNumbers(num1, num2);
+                    methodType = "double";
+                }
                 return true;
             case "/":
             case "divide":
-                return _calculator.TryDivideNumbers(num1, num2, out result);
+                if(useIntVersion)
+                {
+                    return _calculator.TryDivideNumbers((int)num1, (int)num2, out result);
+                }
+                else
+                {
+                    return _calculator.TryDivideNumbers(num1, num2, out result);
+                }
             default:
                 return false;
         }
