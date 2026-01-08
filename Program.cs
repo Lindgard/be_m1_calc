db78518137cebca77e0aa33bb8e092eb7b4e6fbc
ALP <a.b.lindgard@gmail.com>
2026-01-08 12:47:18 +0100

    wrote the remaining needed lines to have the input/output I want for the program. Included error-checks for the various parts

--- Program.cs
+++ Program.cs
@@ -20,6 +20,52 @@ class Program
                 Console.WriteLine("Invalid number. Please try again.");
                 continue;
             }
+
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Enter second number \nPress Enter when done: ");
+            Console.ResetColor();
+            string? input2 = Console.ReadLine();
+
+            if (input2?.ToLower() == "exit") break;
+
+            if (!service.TryParseNumber(input2, out double num2))
+            {
+                Console.WriteLine("Invalid number. Please try again.");
+                continue;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Enter operator (+, -, *, /): ");
+            Console.ResetColor();
+            string? operation = Console.ReadLine();
+
+            if (operation?.ToLower() == "exit") break;
+
+            if (string.IsNullOrEmpty(operation))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid operation. Please try again.");
+                Console.ResetColor();
+                continue;
+            }
+
+            if (!service.PerformCalculation(num1, operation ?? "", num2, out double result))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid operation or division by zero. Please try again.");
+                Console.ResetColor();
+                continue;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Result: {result}");
+                Console.ResetColor();
+                continue;
+            }
+
         }
+
     }
 }
