7aea06d22ba12462ed502ba538d3fdd1fe1c5f7b
ALP <a.b.lindgard@gmail.com>
2026-01-08 12:30:04 +0100

    working on implementing the logic from the class and service into the interactivity of the program

--- Program.cs
+++ Program.cs
@@ -1,7 +1,25 @@
-﻿class Program
+﻿using System.ComponentModel.Design;
+
+class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello world");
+        CalculatorService service = new CalculatorService();
+
+        while (true)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Enter first number \nPress Enter when done: ");
+            Console.ResetColor();
+            string? input1 = Console.ReadLine();
+
+            if (input1?.ToLower() == "exit") break;
+
+            if (!service.TryParseNumber(input1, out double num1))
+            {
+                Console.WriteLine("Invalid number. Please try again.");
+                continue;
+            }
+        }
     }
 }
