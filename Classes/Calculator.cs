66fad3761a8dc70a2d20b21c3ef1cb720461a01f
ALP <a.b.lindgard@gmail.com>
2026-03-10 10:53:28 +0100

    made methods one-liners instead of full bracketswhere applicable

--- Classes/Calculator.cs
+++ Classes/Calculator.cs
@@ -1,17 +1,10 @@
+namespace be_m1_calc.Classes;
+
 public class Calculator
 {
-    public int AddNumbers(int a, int b)
-    {
-        return a + b;
-    }
-    public int SubtractNumbers(int a, int b)
-    {
-        return a - b;
-    }
-    public int MultiplyNumbers(int a, int b)
-    {
-        return a * b;
-    }
+    public int AddNumbers(int a, int b) => a + b;
+    public int SubtractNumbers(int a, int b) => a - b;
+    public int MultiplyNumbers(int a, int b) => a * b;
     public int tryDivideNumbers(int numerator, int denominator)
     {
         if (denominator == 0)
@@ -24,24 +17,15 @@ public class Calculator
         return numerator / denominator;
     }
     //* Double overload av metodene
-    public double AddNumbers(double a, double b)
-    {
-        return a + b;
-    }
-    public double SubtractNumbers(double a, double b)
-    {
-        return a - b;
-    }
-    public double MultiplyNumbers(double a, double b)
-    {
-        return a * b;
-    }
+    public double AddNumbers(double a, double b) => a + b;
+    public double SubtractNumbers(double a, double b) => a - b;
+    public double MultiplyNumbers(double a, double b) => a * b;
     public double tryDivideNumbers(double numerator, double denominator)
     {
         if (denominator == 0)
         {
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Error! Cannot divide by zero(0)!");
+            Console.WriteLine("\u10060 Error! Cannot divide by zero(0)!");
             Console.ResetColor();
             return 0;
         }
